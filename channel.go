6ccf33929e0760e5139f33a5838977fc1a93e3b8
Monika (momo13) <monika.m.moser@googlemail.com>
2014-04-29 15:20:46 +0200

    Reset channels when initializing

--- channel.go
+++ channel.go
@@ -13,12 +13,16 @@ type channel struct {
 	logger  *log.Logger
 }
 
-var channels = map[level]*channel{
-	LEVEL_CRITICAL: &channel{writers: make([]io.Writer, 0, 2), logger: log.New(ioutil.Discard, "C ", log.LstdFlags)},
-	LEVEL_ERROR:    &channel{writers: make([]io.Writer, 0, 2), logger: log.New(ioutil.Discard, "E ", log.LstdFlags)},
-	LEVEL_WARNING:  &channel{writers: make([]io.Writer, 0, 2), logger: log.New(ioutil.Discard, "W ", log.LstdFlags)},
-	LEVEL_INFO:     &channel{writers: make([]io.Writer, 0, 2), logger: log.New(ioutil.Discard, "I ", log.LstdFlags)},
-	LEVEL_DEBUG:    &channel{writers: make([]io.Writer, 0, 2), logger: log.New(ioutil.Discard, "D ", log.LstdFlags)},
+var channels = initChannels()
+
+func initChannels() map[level]*channel {
+	return map[level]*channel{
+		LEVEL_CRITICAL: &channel{writers: make([]io.Writer, 0, 2), logger: log.New(ioutil.Discard, "C ", log.LstdFlags)},
+		LEVEL_ERROR:    &channel{writers: make([]io.Writer, 0, 2), logger: log.New(ioutil.Discard, "E ", log.LstdFlags)},
+		LEVEL_WARNING:  &channel{writers: make([]io.Writer, 0, 2), logger: log.New(ioutil.Discard, "W ", log.LstdFlags)},
+		LEVEL_INFO:     &channel{writers: make([]io.Writer, 0, 2), logger: log.New(ioutil.Discard, "I ", log.LstdFlags)},
+		LEVEL_DEBUG:    &channel{writers: make([]io.Writer, 0, 2), logger: log.New(ioutil.Discard, "D ", log.LstdFlags)},
+	}
 }
 
 func (c *channel) Printf(format string, values ...interface{}) {
